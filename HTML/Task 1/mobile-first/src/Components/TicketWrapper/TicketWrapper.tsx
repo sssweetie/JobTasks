9d990a990f31e068f8cae9b80f316d65eba836e1
sweetie <63711180+sssweetie@users.noreply.github.com>
2022-10-16 17:37:49 +0300

    commit-done

--- HTML/Task 1/mobile-first/src/Components/TicketWrapper/TicketWrapper.tsx
+++ HTML/Task 1/mobile-first/src/Components/TicketWrapper/TicketWrapper.tsx
@@ -18,25 +18,23 @@ const TicketWrapper = ({
   yellow = false,
   img,
 }: Props) => {
-  const [date, setDateSlice] = useState(4);
+  const [date, setDateSlice] = useState(3);
   const labels = [
     "Билет на целый день",
     "Неограниченное число катаний",
     "6 остановок у главных достопримечательностей",
     "Ближайший рейс сегодня",
   ];
-  const times = ["09:00", "12:00", "15:00", "18:00", "21:00", "09:00", "12:00"];
+  const times = ["09:00", "12:00", "15:00", "18:00", "21:00", "24:00", "03:00"];
   const labelsComponent = labels.map((item) => (
     <div className={styles.flagWrapper}>
       <img src={flag}></img>
       <p>{item}</p>
     </div>
   ));
-
   const timesComponent = times.map((item) => {
-    return <div className={styles.raceTime}>{item}</div>;
+    return <p className={styles.raceTime}>{item}</p>;
   });
-
   return (
     <section className={styles.screenWrapper} style={{ marginTop: marginTop }}>
       <div
@@ -61,19 +59,19 @@ const TicketWrapper = ({
           </p>
         </div>
         <div>{labelsComponent}</div>
-        {timesComponent.length > 4 ? (
+        {timesComponent.length > 3 ? (
           <div className={styles.notEnoughTimeWrapper}>
             <div className={styles.timesWrapper}>
               {timesComponent.slice(0, date)}
+              {date < times.length ? (
+                <p
+                  onClick={() => setDateSlice(times.length)}
+                  className={styles.moreTime}
+                >
+                  ещё...
+                </p>
+              ) : null}
             </div>
-            {date < times.length ? (
-              <p
-                onClick={() => setDateSlice(date + 4)}
-                className={styles.moreTime}
-              >
-                ещё...
-              </p>
-            ) : null}
           </div>
         ) : (
           <div className={styles.timesWrapper}>{timesComponent}</div>
