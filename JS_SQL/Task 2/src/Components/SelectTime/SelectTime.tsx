a4a2e4ebebe076ac2cf1d1956f33f81dca501afa
sweetie <63711180+sssweetie@users.noreply.github.com>
2022-10-23 23:50:46 +0300

    commit-no-usestate

--- JS_SQL/Task 2/src/Components/SelectTime/SelectTime.tsx
+++ JS_SQL/Task 2/src/Components/SelectTime/SelectTime.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Total from "../ScoreTickets/Total";
 import styles from "./SelectTime.module.css";
 
